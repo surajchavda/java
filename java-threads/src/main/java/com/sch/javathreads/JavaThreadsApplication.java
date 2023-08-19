7b3dfb4ed66b71a700bb801ab8b21e0545e27983
Suraj Chavda <surajchavda@gmail.com>
2023-08-19 13:48:21 +0530

    Adding initial project files

---
+++ java-threads/src/main/java/com/sch/javathreads/JavaThreadsApplication.java
@@ -0,0 +1,13 @@
+package com.sch.javathreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaThreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaThreadsApplication.class, args);
+	}
+
+}
---
+++ java-threads/src/test/java/com/sch/javathreads/JavaThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sch.javathreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaThreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
